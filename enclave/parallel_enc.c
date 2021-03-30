b679a95c6df39b748ec41d73cc767af082e73a1b
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2021-03-30 11:25:02 -0700

    Spawn threads for sort, set parameters, and synchronize entry

--- enclave/parallel_enc.c
+++ enclave/parallel_enc.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 #include <openenclave/enclave.h>
 #include "parallel_t.h"
+#include "synch.h"
 
-int ecall_main(int world_rank, int world_size) {
-    oe_result_t result;
-    MPI_Status mpi_status;
-    int ret = -1;
+static int world_rank;
+static int world_size;
+static size_t num_threads;
 
-    result = ocall_mpi_send_bytes(&ret, (unsigned char *) &world_rank, 4,
-            world_rank ^ 1, 0);
-    if (result != OE_OK || ret) {
-        goto exit;
+static void wait_for_all_threads(void) {
+    static size_t num_threads_waiting;
+    static condvar_t all_threads_finished;
+    static spinlock_t all_threads_lock;
+
+    spinlock_lock(&all_threads_lock);
+    num_threads_waiting++;
+    if (num_threads_waiting >= num_threads) {
+        condvar_broadcast(&all_threads_finished, &all_threads_lock);
+        num_threads_waiting = 0;
+    } else {
+        condvar_wait(&all_threads_finished, &all_threads_lock);
     }
+    spinlock_unlock(&all_threads_lock);
+}
 
-    int recv;
-    result = ocall_mpi_recv_bytes(&ret, (unsigned char *) &recv, 4, world_rank ^ 1,
-            0, &mpi_status);
-    if (result != OE_OK || ret) {
-        goto exit;
-    }
+void ecall_set_params(int world_rank_, int world_size_, size_t num_threads_) {
+    /* Set global parameters. */
+    world_rank = world_rank_;
+    world_size = world_size_;
+    num_threads = num_threads_;
+}
 
-    printf("Process %d received %d from %d\n", world_rank, recv, world_rank ^ 1);
+void ecall_start_work(size_t thread_id) {
+    static spinlock_t print_lock;
 
-    ret = 0;
+    spinlock_lock(&print_lock);
+    printf("Spawned node %d thread %lu\n", world_rank, thread_id);
+    spinlock_unlock(&print_lock);
+    wait_for_all_threads();
+    spinlock_lock(&print_lock);
+    printf("Released node %d thread %lu\n", world_rank, thread_id);
+    spinlock_unlock(&print_lock);
+}
 
-exit:
-    return ret;
+int ecall_main(void) {
+    /* Start work for this thread. */
+    ecall_start_work(0);
+
+    return 0;
 }
--- host/parallel.c
+++ host/parallel.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <openenclave/host.h>
 #include "parallel_u.h"
@@ -28,14 +29,38 @@ exit:
     return ret;
 }
 
+struct thread_args {
+    oe_enclave_t *enclave;
+    size_t thread_id;
+};
+
+static void *start_thread_work(void *args_) {
+    struct thread_args *args = args_;
+    ecall_start_work(args->enclave, args->thread_id);
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     oe_enclave_t *enclave;
     oe_result_t result;
     int ret = 0;
+    size_t num_threads = 1;
 
     if (argc < 2) {
-        printf("usage: %s enclave_image\n", argv[0]);
+        printf("usage: %s enclave_image [num_threads]\n", argv[0]);
     }
+
+    if (argc >= 3) {
+        int n = atol(argv[2]);
+        if (n < 0) {
+            fprintf(stderr, "Invalid number of threads\n");
+            return ret;
+        }
+        num_threads = n;
+    }
+
+    struct thread_args thread_args[num_threads - 1];
+    pthread_t threads[num_threads - 1];
 
     ret = init_mpi();
 
@@ -58,14 +83,38 @@ int main(int argc, char *argv[]) {
         goto exit;
     }
 
-    ecall_main(enclave, &ret, world_rank, world_size);
+    result = ecall_set_params(enclave, world_rank, world_size, num_threads);
+
+    if (result != OE_OK) {
+        fprintf(stderr, "ecall_set_num_threads: %s\n", oe_result_str(result));
+        goto exit_terminate_enclave;
+    }
+
+    for (size_t i = 1; i < num_threads; i++) {
+        thread_args[i - 1].enclave = enclave;
+        thread_args[i - 1].thread_id = i;
+        ret = pthread_create(&threads[i - 1], NULL, start_thread_work,
+                &thread_args[i - 1]);
+        if (ret) {
+            perror("pthread_create");
+            goto exit_terminate_enclave;
+        }
+    }
+
+    ecall_main(enclave, &ret);
+
+    for (size_t i = 1; i < num_threads; i++) {
+        pthread_join(threads[i - 1], NULL);
+    }
 
     if (ret) {
         fprintf(stderr, "Enclave exited with return code %d\n", ret);
+        goto exit_terminate_enclave;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
+exit_terminate_enclave:
     oe_terminate_enclave(enclave);
 exit:
     return ret;
