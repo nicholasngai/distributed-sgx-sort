e7297867e114de38f5542c3e2d3e9cf9c3e7b53f
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2021-10-15 19:16:07 -0700

    Switch to DTLS for MPI transport encryption

--- enclave/mpi_tls.c
+++ enclave/mpi_tls.c
@@ -17,22 +17,23 @@
 #endif /* DISTRUBTED_SGX_SORT_HOSTONLY */
 #include "enclave/synch.h"
 
+#include <mbedtls/debug.h>
+
 /* Include simulation cert and key data if compiling in simulation mode or
  * hostonly mode. */
 #if defined(OE_SIMULATION) || defined(DISTRIBUTED_SGX_SORT_HOSTONLY)
 #include "enclave/sim_cert.h"
 #endif /* OE_SIMULATION || DISTRIBUTED_SGX_SORT_HOSTONLY */
 
-#define BUFFER_SIZE 8192
-
 struct mpi_tls_session {
     mbedtls_ssl_config conf;
     mbedtls_ssl_context ssl;
     mbedtls_ctr_drbg_context drbg;
+    struct {
+        uint32_t int_ms;
+        uint32_t fin_ms;
+    } timing;
     spinlock_t lock;
-    unsigned char *buffer;
-    size_t buffer_offset;
-    size_t buffer_bytes_left;
 
     /* Parameters used by send and recv callbacks. */
     int rank;
@@ -53,28 +54,21 @@ static int verify_callback(void *data UNUSED, mbedtls_x509_crt *crt UNUSED,
 
 static int send_callback(void *session_, const unsigned char *buf, size_t len) {
     struct mpi_tls_session *session = session_;
-    size_t bytes_remaining = len;
     int ret = -1;
 
-    while (bytes_remaining) {
-        size_t bytes_to_send = MIN(bytes_remaining, BUFFER_SIZE);
 #ifndef DISTRIBUTED_SGX_SORT_HOSTONLY
-        oe_result_t result = ocall_mpi_send_bytes(&ret, buf, bytes_to_send,
-                session->rank, session->tag);
-        if (result != OE_OK) {
-            handle_oe_error(result, "ocall_mpi_send_bytes");
-            goto exit;
-        }
+    oe_result_t result = ocall_mpi_send_bytes(&ret, buf, len, session->rank,
+            session->tag);
+    if (result != OE_OK) {
+        handle_oe_error(result, "ocall_mpi_send_bytes");
+        goto exit;
+    }
 #else /* DISTRUBTED_SGX_SORT_HOSTONLY */
-        ret = ocall_mpi_send_bytes(buf, bytes_to_send, session->rank,
-                session->tag);
+    ret = ocall_mpi_send_bytes(buf, len, session->rank, session->tag);
 #endif /* DISTRUBTED_SGX_SORT_HOSTONLY */
-        if (ret) {
-            handle_error_string("Failed to send TLS encrypted bytes");
-            goto exit;
-        }
-        buf += bytes_to_send;
-        bytes_remaining -= bytes_to_send;
+    if (ret) {
+        handle_error_string("Failed to send TLS encrypted bytes");
+        goto exit;
     }
 
     ret = len;
@@ -86,54 +80,43 @@ exit:
 static int recv_callback(void *session_, unsigned char *buf, size_t len,
         uint32_t timeout UNUSED) {
     struct mpi_tls_session *session = session_;
-    size_t bytes_remaining = len;
     int ret = -1;
-
-    /* Copy excess bytes in the buffer from earlier if we have any. */
-    if (session->buffer_bytes_left) {
-        size_t bytes_to_copy = MIN(bytes_remaining, session->buffer_bytes_left);
-        memcpy(buf, session->buffer + session->buffer_offset, bytes_to_copy);
-        session->buffer_offset += bytes_to_copy;
-        session->buffer_bytes_left -= bytes_to_copy;
-        bytes_remaining -= bytes_to_copy;
-        buf += bytes_to_copy;
-    }
 
     /* If there are bytes remaining afterwards, receive bytes from MPI until MPI
      * indicates blocking. */
-    while (bytes_remaining && ret) {
 #ifndef DISTRIBUTED_SGX_SORT_HOSTONLY
-        oe_result_t result = ocall_mpi_try_recv_bytes(&ret, session->buffer,
-                BUFFER_SIZE, session->rank, session->tag);
-        if (result != OE_OK) {
-            handle_oe_error(result, "ocall_mpi_try_recv_bytes");
-            goto exit;
-        }
+    oe_result_t result = ocall_mpi_try_recv_bytes(&ret, buf, len, session->rank,
+            session->tag);
+    if (result != OE_OK) {
+        handle_oe_error(result, "ocall_mpi_try_recv_bytes");
+        goto exit;
+    }
 #else /* DISTRUBTED_SGX_SORT_HOSTONLY */
-        ret = ocall_mpi_try_recv_bytes(session->buffer, BUFFER_SIZE,
-                session->rank, session->tag);
+    ret = ocall_mpi_try_recv_bytes(buf, len, session->rank, session->tag);
 #endif /* DISTRUBTED_SGX_SORT_HOSTONLY */
-        if (ret < 0) {
-            handle_error_string("Failed to receive TLS encrypted bytes");
-            goto exit;
-        }
-
-        /* Copy as many bytes as requested to the destination buffer. */
-        size_t bytes_to_copy = MIN(bytes_remaining, (size_t) ret);
-        memcpy(buf, session->buffer, bytes_to_copy);
-        session->buffer_offset = bytes_to_copy;
-        session->buffer_bytes_left += ret - bytes_to_copy;
-        bytes_remaining -= bytes_to_copy;
-        buf += bytes_to_copy;
+    if (ret < 0) {
+        handle_error_string("Failed to receive TLS encrypted bytes");
+        goto exit;
     }
 
-    ret = len - bytes_remaining;
     if (!ret) {
         ret = MBEDTLS_ERR_SSL_WANT_READ;
     }
 
 exit:
     return ret;
+}
+
+static void set_timer_callback(void *session_, uint32_t int_ms,
+        uint32_t fin_ms) {
+    struct mpi_tls_session *session = session_;
+    session->timing.int_ms = int_ms;
+    session->timing.fin_ms = fin_ms;
+}
+
+static int get_timer_callback(void *session_) {
+    struct mpi_tls_session *session = session_;
+    return session->timing.fin_ms ? 0 : -1;
 }
 
 static int init_session(struct mpi_tls_session *session, bool is_server,
@@ -156,7 +139,7 @@ static int init_session(struct mpi_tls_session *session, bool is_server,
     mbedtls_ssl_config_init(&session->conf);
     ret = mbedtls_ssl_config_defaults(&session->conf,
             is_server ? MBEDTLS_SSL_IS_SERVER : MBEDTLS_SSL_IS_CLIENT,
-            MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);
+            MBEDTLS_SSL_TRANSPORT_DATAGRAM, MBEDTLS_SSL_PRESET_DEFAULT);
     if (ret) {
         handle_mbedtls_error(ret, "mbedtls_ssl_config_defaults");
         goto exit_free_config;
@@ -170,6 +153,7 @@ static int init_session(struct mpi_tls_session *session, bool is_server,
         handle_mbedtls_error(ret, "mbedtls_ssl_conf_own_cert");
         goto exit_free_config;
     }
+    mbedtls_ssl_conf_dtls_cookies(&session->conf, NULL, NULL, NULL);
 
     /* Initialize SSL. */
     mbedtls_ssl_init(&session->ssl);
@@ -180,18 +164,11 @@ static int init_session(struct mpi_tls_session *session, bool is_server,
     }
     mbedtls_ssl_set_bio(&session->ssl, session, send_callback, NULL,
             recv_callback);
+    mbedtls_ssl_set_timer_cb(&session->ssl, session, set_timer_callback,
+            get_timer_callback);
 
     /* Initialize spinlock. */
     spinlock_init(&session->lock);
-
-    /* Allocate buffer. */
-    session->buffer = malloc(BUFFER_SIZE);
-    if (!session->buffer) {
-        perror("malloc MPI TLS buffer");
-        goto exit_free_ssl;
-    }
-    session->buffer_offset = 0;
-    session->buffer_bytes_left = 0;
 
     /* Initialize rank. */
     session->rank = rank;
@@ -208,7 +185,6 @@ exit_free_drbg:
 }
 
 static void free_session(struct mpi_tls_session *session) {
-    free(session->buffer);
     mbedtls_ctr_drbg_free(&session->drbg);
     mbedtls_ssl_free(&session->ssl);
     mbedtls_ssl_config_free(&session->conf);
@@ -397,20 +373,26 @@ int mpi_tls_send_bytes(const void *buf_, size_t count, int dest, int tag) {
     const unsigned char *buf = buf_;
     int ret = -1;
 
-    size_t bytes_to_write = count;
-    while (bytes_to_write) {
-        spinlock_lock(&sessions[dest].lock);
+    size_t bytes_remaining = count;
+    size_t max_payload_len =
+        mbedtls_ssl_get_max_out_record_payload(&sessions[dest].ssl);
+    spinlock_lock(&sessions[dest].lock);
+    while (bytes_remaining) {
         sessions[dest].tag = tag;
-        ret = mbedtls_ssl_write(&sessions[dest].ssl, buf, count);
-        spinlock_unlock(&sessions[dest].lock);
+        size_t bytes_to_write = MIN(bytes_remaining, max_payload_len);
+        ret = mbedtls_ssl_write(&sessions[dest].ssl, buf, bytes_to_write);
         if (ret < 0) {
+            if (ret == MBEDTLS_ERR_SSL_WANT_READ
+                || ret == MBEDTLS_ERR_SSL_WANT_WRITE) {
+                continue;
+            }
             handle_mbedtls_error(ret, "mbedtls_ssl_write");
-            spinlock_unlock(&sessions[dest].lock);
             goto exit;
         }
         buf += ret;
-        bytes_to_write -= ret;
+        bytes_remaining -= ret;
     }
+    spinlock_unlock(&sessions[dest].lock);
 
     ret = 0;
 
@@ -422,19 +404,26 @@ int mpi_tls_recv_bytes(void *buf_, size_t count, int src, int tag) {
     unsigned char *buf = buf_;
     int ret = -1;
 
-    size_t bytes_to_read = count;
-    while (bytes_to_read) {
-        spinlock_lock(&sessions[src].lock);
+    size_t bytes_remaining = count;
+    size_t max_payload_len =
+        mbedtls_ssl_get_max_out_record_payload(&sessions[src].ssl);
+    spinlock_lock(&sessions[src].lock);
+    while (bytes_remaining) {
         sessions[src].tag = tag;
-        ret = mbedtls_ssl_read(&sessions[src].ssl, buf, count);
-        spinlock_unlock(&sessions[src].lock);
+        size_t bytes_to_read = MIN(bytes_remaining, max_payload_len);
+        ret = mbedtls_ssl_read(&sessions[src].ssl, buf, bytes_to_read);
         if (ret < 0) {
+            if (ret == MBEDTLS_ERR_SSL_WANT_READ
+                || ret == MBEDTLS_ERR_SSL_WANT_WRITE) {
+                continue;
+            }
             handle_mbedtls_error(ret, "mbedtls_ssl_read");
             goto exit;
         }
         buf += ret;
-        bytes_to_read -= ret;
+        bytes_remaining -= ret;
     }
+    spinlock_unlock(&sessions[src].lock);
 
     ret = 0;
 
