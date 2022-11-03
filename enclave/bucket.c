7ee58d828f854fccfcd459d960166a5637dc86f9
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2022-11-03 12:41:06 -0700

    Fix bucket sort request initialization for NULL requests

--- enclave/bucket.c
+++ enclave/bucket.c
@@ -1432,7 +1432,7 @@ static int distributed_sample_partition(void *arr_, void *out_,
     size_t sample_idxs[world_size];
     size_t sample_scan_idxs[world_size];
     mpi_tls_request_t requests[world_size];
-    size_t requests_len = 0;
+    size_t requests_len = world_size;
     ret =
         distributed_quickselect(arr, local_length, local_start, sample_targets,
                 samples, sample_idxs, world_size - 1);
@@ -1494,12 +1494,14 @@ static int distributed_sample_partition(void *arr_, void *out_,
                 ret =
                     mpi_tls_irecv_bytes(&partition_buf[i],
                             elems_to_recv * sizeof(*partition_buf[i]),
-                            MPI_TLS_ANY_SOURCE, 0, &requests[requests_len]);
+                            MPI_TLS_ANY_SOURCE, 0, &requests[i]);
                 if (ret) {
                     handle_error_string("Error receiving partitioned data");
                     goto exit_free_buf;
                 }
-                requests_len++;
+            } else {
+                requests[i].type = MPI_TLS_NULL;
+                requests_len--;
             }
         } else {
             if (sample_scan_idxs[i] < sample_idxs[i]) {
@@ -1526,13 +1528,14 @@ static int distributed_sample_partition(void *arr_, void *out_,
                 ret =
                     mpi_tls_isend_bytes(&partition_buf[i],
                             elems_to_send * sizeof(*partition_buf[i]), i, 0,
-                            &requests[requests_len]);
+                            &requests[i]);
                 if (ret) {
                     handle_error_string("Error sending partitioned data");
                     goto exit_free_buf;
                 }
-
-                requests_len++;
+            } else {
+                requests[i].type = MPI_TLS_NULL;
+                requests_len--;
             }
         }
     }
