4978cb9772aeb42feb6af99eeeb224c8d4559dc6
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2022-11-22 20:06:57 -0800

    Remove usage of external memory from orshuffle sort

--- enclave/orshuffle.h
+++ enclave/orshuffle.h
@@ -2,9 +2,10 @@
 #define DISTRIBUTED_SGX_SORT_ENCLAVE_ORSHUFFLE_H
 
 #include <stddef.h>
+#include "common/elem_t.h"
 
 int orshuffle_init(void);
 void orshuffle_free(void);
-int orshuffle_sort(void *arr, size_t length, size_t num_threads);
+int orshuffle_sort(elem_t *arr, size_t length, size_t num_threads);
 
 #endif
