1e173940a57f5e003828cf1b935075f4e4e0b845
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-02-24 10:23:17 -0800

    Delete resources in destroy-vm.sh script rather than entire group

--- scripts/az/destroy-vm.sh
+++ scripts/az/destroy-vm.sh
@@ -6,4 +6,8 @@ cd "$(dirname "$0")"
 
 . ./common.sh
 
-az group delete -g "$GROUP" -y
+# For some reason, disks don't show up with az resource list, so the disks query
+# is done separately.
+az resource delete --ids \
+    $(az resource list -g "$GROUP" --query '[].id' -o tsv) \
+    $(az disk list -g "$GROUP" --query '[].id' -o tsv)
