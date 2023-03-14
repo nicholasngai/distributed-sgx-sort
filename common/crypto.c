ea9a7bfb57946978ec151470b235d9b97e1909e3
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-03-14 09:04:00 -0700

    Refactor thread_local_ctx and conditional compilation in common/crypto.c

--- common/crypto.c
+++ common/crypto.c
@@ -1,9 +1,9 @@
 #include "crypto.h"
-#include <stdbool.h>
 #include <stddef.h>
 #include <threads.h>
 #include <mbedtls/gcm.h>
 #include <mbedtls/entropy.h>
+#include "common/defs.h"
 #include "common/error.h"
 
 #ifdef DISTRIBUTED_SGX_SORT_NORDRAND
@@ -14,36 +14,19 @@
 
 mbedtls_entropy_context entropy_ctx;
 
-#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
 struct thread_local_ctx {
+#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
     mbedtls_ctr_drbg_context drbg_ctx;
+#endif
     struct thread_local_ctx **ptr;
 };
 
 static struct thread_local_ctx ctxs[THREAD_LOCAL_LIST_MAXLEN];
 static size_t ctx_len;
 static thread_local struct thread_local_ctx *ctx;
-#endif
 
-int rand_init(void) {
-    mbedtls_entropy_init(&entropy_ctx);
-    return 0;
-}
-
-void rand_free(void) {
-#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
-    for (size_t i = 0; i < ctx_len; i++) {
-        mbedtls_ctr_drbg_free(&ctxs[i].drbg_ctx);
-        ctxs[i].ptr = NULL;
-    }
-    ctx_len = 0;
-#endif
-    mbedtls_entropy_free(&entropy_ctx);
-}
-
-#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
-int rand_read(void *buf, size_t n) {
-    int ret = -1;
+static int UNUSED ensure_thread_local_ctx_init(void) {
+    int ret;
 
     if (!ctx || !ctx->ptr) {
         size_t idx =
@@ -51,21 +34,55 @@ int rand_read(void *buf, size_t n) {
         if (ctx_len >= THREAD_LOCAL_LIST_MAXLEN) {
             handle_error_string("Too many threads for crypto");
             __atomic_fetch_sub(&ctx_len, 1, __ATOMIC_RELAXED);
+            ret = -1;
             goto exit;
         }
         ctx = &ctxs[idx];
+        ctx->ptr = &ctx;
 
+#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
         mbedtls_ctr_drbg_init(&ctx->drbg_ctx);
         ret =
-            mbedtls_ctr_drbg_seed(&ctx->drbg_ctx, mbedtls_entropy_func, &entropy_ctx,
-                    NULL, 0);
+            mbedtls_ctr_drbg_seed(&ctx->drbg_ctx, mbedtls_entropy_func,
+                    &entropy_ctx, NULL, 0);
         if (ret) {
             handle_mbedtls_error(ret, "mbedtls_ctr_drbg_seed");
             mbedtls_ctr_drbg_free(&ctx->drbg_ctx);
             __atomic_fetch_sub(&ctx_len, 1, __ATOMIC_RELAXED);
             goto exit;
         }
-        ctx->ptr = &ctx;
+#endif
+    }
+
+    ret = 0;
+
+exit:
+    return ret;
+}
+
+int rand_init(void) {
+    mbedtls_entropy_init(&entropy_ctx);
+    return 0;
+}
+
+void rand_free(void) {
+    for (size_t i = 0; i < ctx_len; i++) {
+#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
+        mbedtls_ctr_drbg_free(&ctxs[i].drbg_ctx);
+#endif
+        ctxs[i].ptr = NULL;
+    }
+    ctx_len = 0;
+    mbedtls_entropy_free(&entropy_ctx);
+}
+
+#ifdef DISTRIBUTED_SGX_SORT_NORDRAND
+int rand_read(void *buf, size_t n) {
+    int ret;
+
+    ret = ensure_thread_local_ctx_init();
+    if (ret) {
+        goto exit;
     }
 
     ret = mbedtls_ctr_drbg_random(&ctx->drbg_ctx, buf, n);
