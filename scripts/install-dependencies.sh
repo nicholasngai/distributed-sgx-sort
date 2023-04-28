4a72c4a4358f3bc1b21d61e99ffe9be3f7d94b3f
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-27 18:08:33 -0700

    Use repo-specific keyrings for install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,18 +4,26 @@ set -eux
 
 sudo apt install -y curl
 
+sudo mkdir -p /etc/apt/keyrings
+
 if ! [ -f /etc/apt/sources.list.d/intel-sgx.list ]; then
-    echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+    echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/intel-sgx-deb.gpg] https://download.01.org/intel-sgx/sgx_repo/ubuntu focal main' \
+        | sudo tee /etc/apt/sources.list.d/intel-sgx.list
 fi
-if ! [ -f /etc/apt/trusted.gpg.d/intel-sgx-deb.asc ]; then
-    sudo curl -Lo /etc/apt/trusted.gpg.d/intel-sgx-deb.asc https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
+if ! [ -f /etc/apt/keyrings/intel-sgx-deb.gpg ]; then
+    curl -fsSL https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key \
+        | gpg --dearmor \
+        | sudo tee /etc/apt/keyrings/intel-sgx-deb.gpg >/dev/null
 fi
 
 if ! [ -f /etc/apt/sources.list.d/msprod.list ]; then
-    echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/20.04/prod focal main" | sudo tee /etc/apt/sources.list.d/msprod.list
+    echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/20.04/prod focal main' \
+        | sudo tee /etc/apt/sources.list.d/msprod.list
 fi
-if ! [ -f /etc/apt/trusted.gpg.d/microsoft.asc ]; then
-    sudo curl -Lo /etc/apt/trusted.gpg.d/microsoft.asc https://packages.microsoft.com/keys/microsoft.asc
+if ! [ -f /etc/apt/keyrings/microsoft.gpg ]; then
+    curl -fsSL /etc/apt/trusted.gpg.d/microsoft.asc https://packages.microsoft.com/keys/microsoft.asc \
+        | gpg --dearmor \
+        | sudo tee /etc/apt/keyrings/microsoft.gpg >/dev/null
 fi
 
 sudo apt update
